9abb6664ded8b636b04ac11f0183b1801b78668c
MannesdeGroot <97058263@st.deltion.nl>
2019-11-11 16:57:01 +0100

    AAAAAAAAAA?

---
+++ VR escaper room/Assets/Mannes/Scripts/Lock.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public int[] code;
+    public int[] enteredCode;
+    private int valuesEntered;
+
+    void Start()
+    {
+        for (int i = 0; i < code.Length; i++)
+        {
+            code[i] = Random.Range(0, 9);
+        }
+    }
+
+    public void AddValue(int value)
+    {
+        if (valuesEntered < enteredCode.Length)
+        {
+            enteredCode[valuesEntered] = value;
+            valuesEntered++;
+        }
+        else if (valuesEntered == enteredCode.Length)
+        {
+            CompareCode();
+        }
+    }
+
+    void CompareCode()
+    {
+        bool codeCorrect = true;
+        for (int i = 0; i < code.Length; i++)
+        {
+            if(code[i] != enteredCode[i])
+            {
+                codeCorrect = false;
+            }
+        }
+        if (codeCorrect)
+        {
+            Open();
+        }
+    }
+
+    void Open()
+    {
+        print("Safe unlocked");
+    }
+}
---
+++ VR escaper room/Assets/Mannes/Scripts/LockKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockKey : MonoBehaviour
+{
+    private Lock lockObj;
+    public int value;
+
+    private void Start()
+    {
+        lockObj = GetComponentInParent<Lock>();
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            print(value);
+            lockObj.AddValue(value);
+        }
+    }
+}
