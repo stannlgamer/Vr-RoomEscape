74e15793b52913c4f3c1a0a915b81a51bdce3460
AnthonieP <anthonie.pap111@gmail.com>
2019-11-13 13:58:38 +0100

    Grabbing

---
+++ VR escaper room/Assets/Anthonie/Grabbing.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Grabbing : MonoBehaviour
+{
+    public float grabRadius;
+    public string inputName;
+    GameObject holding;
+    public float trowMultiplier;
+    public XRNode NodeType;
+    Vector3 lastFramePos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.localPosition = InputTracking.GetLocalPosition(NodeType);
+        transform.localRotation = InputTracking.GetLocalRotation(NodeType);
+
+
+        if (Input.GetButtonDown(inputName))
+        {
+            Grab();
+        }
+        else if (Input.GetButtonUp(inputName))
+        {
+            LetGo();
+        }
+        else if(holding != null)
+        {
+            bool holdingB = false;
+            Collider[] colliders = Physics.OverlapSphere(transform.position, grabRadius);
+            for (int i = 0; i < colliders.Length && !holdingB; i++)
+            {
+                if(colliders[i].gameObject == holding)
+                {
+                    holdingB = true;
+                }
+                else if(i == colliders.Length - 1 && !holdingB)
+                {
+                    LetGo();
+                }
+            }
+        }
+    }
+
+    void Grab()
+    {
+        bool grab = false;
+        Collider[] colliders = Physics.OverlapSphere(transform.position, grabRadius);
+
+        for (int i = 0; i < colliders.Length && !grab; i++)
+        {
+            if(colliders[i].transform.tag == ("Grab"))
+            {
+                //lock position.
+                var col = colliders[i].transform;
+                col.GetComponent<Rigidbody>().useGravity = false;
+                //make child of hand.
+                col.parent = transform;
+                holding = colliders[i].gameObject;
+                grab = true;
+            }
+
+
+        }
+    }
+
+    void LetGo()
+    {
+        holding.GetComponent<Rigidbody>().useGravity = true;
+        holding.transform.parent = null;
+        Vector3 CurrentVelocity = (transform.position - lastFramePos) / Time.deltaTime;
+        holding.GetComponent<Rigidbody>().velocity = CurrentVelocity * trowMultiplier;
+        holding = null;
+        lastFramePos = transform.position;
+    }
+}
---
+++ VR escaper room/Assets/Anthonie/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public bool test;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
