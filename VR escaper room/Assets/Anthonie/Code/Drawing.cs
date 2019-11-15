ad041c2a6e0cc28856dc1de3857c728c34fa2110
AnthonieP <anthonie.pap111@gmail.com>
2019-11-15 15:44:22 +0100

    dsfdef

---
+++ VR escaper room/Assets/Anthonie/Code/Drawing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour
+{
+    RaycastHit ray;
+    public float teleportRange;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        if(Physics.Raycast(transform.position, transform.forward, out ray, teleportRange))
+        {
+            if (ray.transform.tag == "Drawable")
+            {
+
+            }
+        }
+
+
+
+    }
+}
