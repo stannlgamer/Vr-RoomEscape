3dd5da688d82e7ed3d4a94bd956099b76a13bc6f
MannesdeGroot <97058263@st.deltion.nl>
2019-11-20 13:49:09 +0100

    aasdfghjhgfdsafdhjft

--- VR escaper room/Assets/Mannes/Scripts/Target.cs
+++ VR escaper room/Assets/Mannes/Scripts/Target.cs
@@ -10,7 +10,6 @@ public class Target : MonoBehaviour
         {
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
             gameObject.tag = "Grab";
-            print("Hit!");
         }
     }
 }
