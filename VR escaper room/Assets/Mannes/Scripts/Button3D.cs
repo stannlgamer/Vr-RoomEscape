bad51b7cd43661def1a433dda95ee138d0aec5d7
MannesdeGroot <97058263@st.deltion.nl>
2019-11-21 10:19:58 +0100

    Summary (required)

---
+++ VR escaper room/Assets/Mannes/Scripts/Button3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button3D : MonoBehaviour
+{
+    public UnityEvent onPress;
+
+    void OnPress()
+    {
+        onPress.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            OnPress();
+        }
+    }
+}
