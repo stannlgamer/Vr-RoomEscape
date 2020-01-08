9f0a90743fc3183ce6bcde70af584b8f1ab06e40
MannesdeGroot <97058263@st.deltion.nl>
2020-01-08 11:36:57 +0100

    Radio

---
+++ VR escaper room/Assets/Mannes/Scripts/Radio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+    private bool playing;
+
+    public void PowerButton()
+    {
+        playing = !playing;
+
+        if (playing)
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            gameObject.GetComponent<AudioSource>().Stop();
+        }
+    }
+}
